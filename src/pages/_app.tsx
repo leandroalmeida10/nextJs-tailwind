cdef2656faa0bfd7cc9288bfa4a9b6e7ed303a16
Leandro Almeida <lalmeida@somoslogin.com.br>
2023-04-24 15:27:56 -0300

    Subindo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app'
+import '../../public/globals.css'
+import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
