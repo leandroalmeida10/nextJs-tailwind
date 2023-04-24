d37ec694937f5d53cc00a459dea4105923cc57fb
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-04-24 15:44:00 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
-import '../../public/globals.css'
+import '../styles/global.css'
+
 import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
